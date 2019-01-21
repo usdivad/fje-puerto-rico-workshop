b8628b3b436449756f20fab4902e752e23c2ec65
David Su <david.d.su@gmail.com>
2019-01-21 14:43:03 -0500

    implement win logic (and final chord) for sfx example

--- 3_placesound/sketch.js
+++ 3_placesound/sketch.js
@@ -13,6 +13,8 @@ var board = [
 
 var currMarker = "x";
 
+var winner = {"positions": [], "marker": " "};
+
 // var voiceX = new Tone.Synth({"oscillator": {"type": "triangle"}});
 // var voiceO = new Tone.Synth({"oscillator": {"type": "fmtriangle"}});
 
@@ -54,13 +56,16 @@ function draw() {
 	currMarkerThickness = Math.max(0, currMarkerThickness - 1);
 
 	// Mouse hover
-	strokeWeight(1);
-	drawMarkerHover();
+	if (!isGameOver()) {
+		strokeWeight(1);
+		drawMarkerHover();
+	}
 }
 
 function mousePressed() {
-	placeMarker(mouseX, mouseY);
-}
+	if (!isGameOver()) {
+		placeMarker(mouseX, mouseY);
+	}}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -74,6 +79,17 @@ function drawBoardCell(i, marker) {
 	var posY = (Math.floor(i/3) * height/3) + (height/6);
 
 	if (marker == "x") {
+		// Winning markers
+		if (marker == winner["marker"] && winner["positions"].indexOf(i) >= 0) {
+			strokeWeight(24);
+			stroke("#0a9");
+
+			line(posX - markerSize/16, posY - markerSize/16, posX + markerSize/16, posY + markerSize/16);
+			line(posX + markerSize/16, posY - markerSize/16, posX - markerSize/16, posY + markerSize/16);
+		}
+
+
+		strokeWeight(6);
 		stroke("#0fa");
 
 		// Diagonal lines
@@ -81,6 +97,15 @@ function drawBoardCell(i, marker) {
 		line(posX + markerSize/16, posY - markerSize/16, posX - markerSize/16, posY + markerSize/16);
 	}
 	else if (marker == "o") {
+		// Winning markers
+		if (marker == winner["marker"] && winner["positions"].indexOf(i) >= 0) {
+			strokeWeight(24);
+			stroke("#09a");
+
+			ellipse(posX, posY, markerSize/8, markerSize/8);
+		}
+
+		strokeWeight(6);
 		stroke("#0af");
 		ellipse(posX, posY, markerSize/8, markerSize/8);
 	}
@@ -92,6 +117,7 @@ function placeMarker(x, y) {
 
 	if (!isBoardPositionOccupied(i)) {
 		board[i] = currMarker; // Place
+		winner = determineWinner(); // Determine winner
 		playMarkerPlacementSound(currMarker, i);
 		currMarkerThickness = maxMarkerThickness;
 		currMarker = currMarker == "x" ? "o" : "x"; // Switch marker
@@ -127,11 +153,81 @@ function isBoardPositionOccupied(i) {
 	return board[i] != " ";
 }
 
+// Determine winner of the game
+function determineWinner() {
+	// var isGameOver = false;
+	var winningPositions = [];
+	var winningMarker = " ";
+
+	// Set up position indices and steps
+	var horizontalPositions = [0, 3, 6];
+	var horizontalStep = 1;
+
+	var verticalPositions = [0, 1, 2];
+	var verticalStep = 3;
+
+	var diagonalTopLeftPositions = [0];
+	var diagonalTopLeftStep = 4;
+
+	var diagonalTopRightPositions = [2];
+	var diagonalTopRightStep = 2;
+
+	// Composite horizontal, vertical, diagonal
+	var dataToCheck = [
+		{"positions": horizontalPositions, "step": horizontalStep},
+		{"positions": verticalPositions, "step": verticalStep},
+		{"positions": diagonalTopLeftPositions, "step": diagonalTopLeftStep},
+		{"positions": diagonalTopRightPositions, "step": diagonalTopRightStep},
+	];
+
+	// Check all possible winning combos
+	for (var positionStepPairIndex=0; positionStepPairIndex < dataToCheck.length; positionStepPairIndex++) {
+		var positions = dataToCheck[positionStepPairIndex]["positions"];
+		var step = dataToCheck[positionStepPairIndex]["step"];
+
+		// Each starting position for h, v, d
+		for (var startingPositionIndex=0; startingPositionIndex < positions.length; startingPositionIndex++) {
+			var startingPosition = positions[startingPositionIndex];
+			if (board[startingPosition] == board[startingPosition + step] &&
+				board[startingPosition] == board[startingPosition + (step*2)] &&
+				board[startingPosition] != " ") {
+				winningPositions = [startingPosition,
+									startingPosition + step,
+									startingPosition + (step*2)];
+				winningMarker = board[startingPosition];
+				console.log("We have a winner!");
+			}
+		}
+	}
+
+	console.log("winningPositions:");
+	console.log(winningPositions);
+	console.log("winningMarker = " + winningMarker);
+
+	return {"positions": winningPositions, "marker": winningMarker};
+}
+
+// Checks if game is over
+function isGameOver() {
+	// console.log(winner);
+	return winner["marker"] != " ";
+}
+
 function playMarkerPlacementSound(marker, i) {
 	if (currMarker == "x") {
-		synthX.triggerAttackRelease(["C4", "E4"], "8n");
+		if (winner["marker"] == currMarker) {
+			synthX.triggerAttackRelease(["C4", "E4", "G4", "C5"], "8n");	
+		}
+		else {
+			synthX.triggerAttackRelease(["C4", "E4"], "8n");
+		}
 	}
 	else if (currMarker == "o") {
-		synthO.triggerAttackRelease(["E4", "G4"], "8n");
+		if (winner["marker"] == currMarker) {
+			synthO.triggerAttackRelease(["C4", "E4", "G4", "C5"], "8n");	
+		}
+		else {
+			synthO.triggerAttackRelease(["E4", "G4"], "8n");
+		}
 	}
 }
